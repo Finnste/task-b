c2c30abc359386b2e11049251a96103c6a3fc663
Finnste <Finn.Steffens@gmx.de>
2023-10-25 02:19:53 +0100

    add more fields + styling

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,35 +4,40 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { string, z } from "zod";
 
-const phoneRegex = new RegExp(
-  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
-);
-
 const schema = z.object({
-  name: string().min(3, { message: "Name is required" }),
-  email: string().email(),
+  name: string().min(3, { message: "A name is required" }),
+  email: string().email({ message: "Add a valid email address" }),
+  address: string().min(5, { message: "Add a valid address" }),
+  phone: string(),
+  dob: z.coerce.date()
 });
 
 const Form = () => {
-  const { register, control, handleSubmit, formState } = useForm({
+  const { register, control, handleSubmit, formState, reset } = useForm({
     resolver: zodResolver(schema),
     mode: "onChange",
   });
+
   const { errors } = formState;
 
+  // variable to control if the submission and server response is successfull
   const [success, setSuccess] = useState(false);
+  const [modalText, setModalText] = useState("");
 
+  // function executed when form is submitted
   const handleSave = (formValues) => {
     console.log(formValues);
-
     fetch("https://d21af321-6b82-426d-99e7-fbb4ac7aed40.mock.pstmn.io/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name: "Finn",
-        email: "hello@finn-steffens.de",
+        name: formValues.name,
+        email: formValues.email,
+        address: formValues.address,
+        phone: formValues.phone,
+        dob: formValues.dob,
       }),
     })
       .then((response) => response.json())
@@ -40,12 +45,17 @@ const Form = () => {
         console.log(JSON.stringify(responseData));
         if (responseData.Success === "true") {
           setSuccess(true);
+          setModalText("The user has been successfully added!");
+          reset();
+        } else {
+          setSuccess(true);
+          setModalText("The user was not added!");
         }
       });
   };
 
   return (
-    <div>
+    <div className="wrapper">
       <h1>Please Register Now</h1>
       <form onSubmit={handleSubmit(handleSave)}>
         <div className="fieldContainer">
@@ -54,31 +64,59 @@ const Form = () => {
             type="text"
             {...register("name")}
             placeholder="Name"
-            className={errors.name ? "input inputError" : "input"}
           />
           <div className="alert">{errors.name?.message}</div>
         </div>
 
         <div className="fieldContainer">
-          <p>Email Adress</p>
+          <p>Email Address</p>
           <input
             type="email"
             {...register("email")}
             placeholder="Email"
-            className={errors.email ? "input inputError" : "input"}
           />
           <div className="alert">{errors.email?.message}</div>
         </div>
 
-        <button
-          className={errors ? "submitButton buttonError" : "submitButton"}
-          type="submit"
-        >
+        <div className="fieldContainer">
+          <p>Address</p>
+          <input
+            type="text"
+            {...register("address")}
+            placeholder="Address"
+          />
+          <div className="alert">{errors.address?.message}</div>
+        </div>
+
+        <div className="fieldContainer">
+          <p>Phone Number</p>
+          <input
+            type="text"
+            {...register("phone")}
+            placeholder="Phone Number"
+          />
+          <div className="alert">{errors.phone?.message}</div>
+        </div>
+
+        <div className="fieldContainer">
+          <p>Date of Birth</p>
+          <input
+            type="date"
+            {...register("dob")}
+            required
+          />
+          <div className="alert">{errors.dob?.message}</div>
+        </div>
+
+        <button className="submitButton" type="submit">
           Submit!
         </button>
       </form>
-      <div className={success ? "modal hideModal" : "modal"}>
-        The user has been successfully created!
+      <div
+        className={!success ? "modal hideModal" : "modal"}
+        onClick={() => setSuccess(false)}
+      >
+        {modalText}
       </div>
     </div>
   );
